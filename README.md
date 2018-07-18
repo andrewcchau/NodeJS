8be19f13df35e5fd99be7047ebfebd09524acd06
Andrew <andrewcchau@gmail.com>
2018-07-18 13:22:33 -0500

    Adding html and main.js file. Added items to README

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
+# Onboarding-UI
+A simple UI for the onboarding Dropwizard Application
+
+# Getting Started
+Before you start, make sure you have done these few things:
+* Have an up-to-date version of NodeJS installed on your computer
+
 # How To Run
-To run the server, run the following commands: ```node src/js/main.js```
+To run the server, run the following command: ```node src/js/main.js```
 
 # Node Version
 This project is currently running on Node v10.6.0
---
+++ src/js/main.js
@@ -0,0 +1,20 @@
+const PORT=9000;
+
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+
+var htmlPath = path.join(__dirname, '..', 'index.html');
+
+fs.readFile(htmlPath, function (err, html) {
+
+    if(err) throw err;
+
+    http.createServer(function (req, res) {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.write(html);
+        res.end();
+    }).listen(PORT);
+});
+
+console.log('Server running at http://localhost:9000/');
