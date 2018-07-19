819a4d42884320c31ae09f12345cebfa9eba2de8
Andrew <andrewcchau@gmail.com>
2018-07-19 13:25:01 -0500

    Added css for alternating background color

--- src/js/main.js
+++ src/js/main.js
@@ -10,16 +10,23 @@ http.createServer(function (req, res) {
     var pathname = url.parse(req.url).pathname;
     var htmlPath = path.join(__dirname, '..', 'index.html');
     var scriptPath = path.join(__dirname, 'script.js');
+    var cssPath = path.join(__dirname, '..', 'css', 'styles.css');
 
-    res.writeHead(200, {'Content-Type': 'text/html'});
+    console.log("Received request for " + pathname);
 
     // router for requests
     if(pathname == "/") {
+        res.writeHead(200, {'Content-Type': 'text/html'});
         var html = fs.readFileSync(htmlPath);
         res.write(html);
     } else if(pathname == "/js/script.js") {
+        res.writeHead(200, {'Content-Type': 'text/javascript'});
         var script = fs.readFileSync(scriptPath);
         res.write(script);
+    } else if(pathname == "/css/styles.css") {
+        res.writeHead(200, {'Content-Type': 'text/css'});
+        var css = fs.readFileSync(cssPath);
+        res.write(css);
     }
 
     res.end();
--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,13 @@
+function isOdd(n) {
+    return Math.abs(n % 2) == 1;
+}
+
 function apiCall() {
     var xhttp = new XMLHttpRequest();
 
     xhttp.onreadystatechange = function() {
         if(this.readyState == xhttp.DONE && this.status == 200) {
-            document.getElementById("data").innerHTML = this.responseText;
+            blockify(this.responseText);
         } else if(this.readyState == xhttp.OPENED || this.readyState == xhttp.HEADERS_RECEIVED
                     || this.readyState == xhttp.LOADING) {
             document.getElementById("data").innerHTML = "Pending . . .";
@@ -14,4 +18,23 @@ function apiCall() {
 
     xhttp.open("GET", "http://localhost:8080/api/1.0/twitter/timeline", true);
     xhttp.send();
+}
+
+function blockify(s) {
+    var json = JSON.parse(s);
+
+    console.log(json);
+    for(var i in json) {
+        var newDiv = document.createElement("div");
+        newDiv.className = "item";
+        var content = document.createTextNode(new Date(json[i].createdAt).toLocaleString() + " : " + json[i].twitterMessage);
+
+        newDiv.appendChild(content);
+
+        var currentDiv = document.getElementById("dataInsert");
+        if(isOdd(i)) {
+            document.body.insertBefore(newDiv, currentDiv);
+        }
+        document.body.insertBefore(newDiv, currentDiv);
+    }
 }
