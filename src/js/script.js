ed3fd8e280ad216baae6310adfae057b0313e276
Andrew <andrewcchau@gmail.com>
2018-07-20 13:30:45 -0500

    Changed fonts for some elements and fixed hyperlink

--- src/js/script.js
+++ src/js/script.js
@@ -25,26 +25,53 @@ function blockify(s) {
     var json = JSON.parse(s);
 
     for(var i in json) {
-        var newDiv = document.createElement("div");
-        var hyperlink = document.createElement("a");
-        var img = document.createElement("img");
-        var span = document.createElement("span");
-        var date = document.createTextNode(new Date(json[i].createdAt).toLocaleString() + " : ");
-        var content = document.createTextNode(json[i].twitterMessage);
+        var divContainer = document.createElement("div");
+        var userContainer = document.createElement("div");
+        var messageContainer = document.createElement("div");
 
+        var img = document.createElement("img");
+        var handleContainer = document.createElement("div");
+        var nameContainer = document.createElement("div");
+        var dateContainer = document.createElement("div");
+        var hyperlink = document.createElement("a");
+
+        var date = document.createTextNode(new Date(json[i].createdAt).toLocaleString());
+        var content = document.createTextNode(json[i].twitterMessage);
+        var handleText = document.createTextNode(json[i].user.twitterHandle);
+        var nameText = document.createTextNode(json[i].user.name);
+
+        /* Container for the user info */
         img.src = json[i].user.profileImageURL;
 
-        span.appendChild(date);
+        handleContainer.appendChild(handleText);
+        handleContainer.className = "twitterHandle";
 
-        hyperlink.href = "https://twitter.com/" + json[i].user.name + "/status/" + json[i].Id;
+        nameContainer.appendChild(nameText);
+        nameContainer.className = "twitterName";
+
+        userContainer.className = "user";
+        userContainer.appendChild(img);
+        userContainer.appendChild(handleContainer);
+        userContainer.appendChild(nameContainer);
+
+        /* Container for the message data */
+        dateContainer.className = "date";
+        dateContainer.appendChild(date);
+
+        hyperlink.className = "messageText";
+        hyperlink.href = "https://twitter.com/" + json[i].user.name + "/status/" + json[i].id;
         hyperlink.target = "_blank";
         hyperlink.appendChild(content);
 
-        newDiv.className = "item";
-        newDiv.appendChild(img);
-        newDiv.appendChild(span);
-        newDiv.appendChild(hyperlink);
+        messageContainer.className = "message";
+        messageContainer.appendChild(dateContainer);
+        messageContainer.appendChild(hyperlink);
 
-        document.getElementById("data").appendChild(newDiv);
+        /* Place containers into html*/
+        divContainer.className = "item";
+        divContainer.appendChild(userContainer);
+        divContainer.appendChild(messageContainer);
+
+        document.getElementById("data").appendChild(divContainer);
     }
 }
