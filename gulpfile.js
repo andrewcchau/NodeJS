8eeb587b7b380df43dce9fe421d8b2a400f52658
Andrew <andrewcchau@gmail.com>
2018-07-25 15:06:09 -0500

    Added babel and browserify to package.json and added code to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -24,3 +24,38 @@ gulp.task('sass', function() {
 
 /* GULP command that executes everything */
 gulp.task('dev', ['sass', 'server']);
+
+function bundle(b) {
+    return b.bundle()
+        .on('error', function (err) {
+            gutil.log(err.toString());
+            this.emit('end');
+        })
+        .pipe(source('bundle.js'))
+        .pipe(buffer())
+        .pipe(sourcemaps.init({loadMaps: true}))
+// Add transformation tasks to the pipeline here.
+//        .pipe(config.production ? uglify() : gulpUtil.noop())
+        .pipe(sourcemaps.write('./'))
+//        .pipe(gulp.dest('./build' + config.versionPath + '/js/'));
+        .pipe(gulp.dest('./dist/js/'));
+}
+
+
+gulp.task('js', function() {
+    return browserify({
+        debug: true,
+        entries: ['./src/js/main.js'],
+        paths: ['./src/js', './node_modules'],
+        cache: {},
+        packageCache: {}
+    }).transform(babelify)
+        .bundle()
+        .on('error', function (err) {
+            gutil.log(err.toString());
+            this.emit('end');
+        })
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest('./dist/js/'))
+        .pipe(connect.reload());
+});
