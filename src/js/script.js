f8e1c50ce10f81b99d25ed88e63a610f9709a417
Andrew <andrewcchau@gmail.com>
2018-07-27 15:47:35 -0500

    React code for timeline

--- src/js/script.js
+++ src/js/script.js
@@ -15,6 +15,12 @@ const init = () => {
     apiCall();
 }
 
+class Button extends React.Component {
+    render() {
+        return e('button', { className: "timelineButton" }, this.props.buttonMessage);
+    }
+}
+
 class Status extends React.Component {
     render() {
         return e('div', { className: this.props.className }, this.props.message);
@@ -48,85 +54,55 @@ const apiCall = () => {
     xhttp.send();
 }
 
-class MessageDate extends React.Component {
-    render() {
-        return e('div', { className: "date" }, this.props.date);
+const MessageDate = (date) => {
+    let msgDate;
+    /* Parse the date into human-readable format */
+    if(date != null) {
+        let formatter = new Intl.DateTimeFormat("en-US", { month: "short" });
+        msgDate = new Date(date);
+        msgDate = formatter.format(msgDate) + " " + msgDate.getUTCDate();
     }
+
+    return e('div', { className: "date" }, msgDate);
 }
 
-class MessageLink extends React.Component {
-    render() {
-        return e('a', { className: "messageText" , href: this.props.link, target: this.props.target },
-            this.props.message);
-    }
+const MessageLink = (user, id, message) => {
+    let link = "https://twitter.com/" + user + "/status/" + id;
+
+    return e('a', { className: "messageText", href: link, target: "_blank" }, message);
 }
 
-class Message extends React.Component {
-    render() {
-        return e('div', { className: "message" },
-            e(MessageDate, { date: this.props.date, target: this.props.target }, null ),
-            e(MessageLink, { link: this.props.link, message: this.props.message }, null)
-        );
-    }
+const Message = (jsonObj) => {
+    return e('div', { className: "message" },
+        MessageDate(jsonObj.createdAt),
+        MessageLink(jsonObj.user.name, jsonObj.id, jsonObj.twitterMessage));
 }
 
-class User extends React.Component {
-    render() {
-        return e('div', { className: "user" },
-            e('img', { src: this.props.srcURL }, null),
-            e('div', { className: "twitterHandle" }, this.props.handle),
-            e('div', { className: "twitterName" }, this.props.name)
-        );
-    }
+const Image = (src) => {
+    return e('img', { src: src }, null);
 }
 
-class Tweet extends React.Component {
+const Handle = (handle) => {
+    return e('div', { className: "twitterHandle" }, handle);
+}
+
+const Name = (name) => {
+    return e('div', { className: "twitterName" }, name);
+}
+
+const User = (user) => {
+    return e('div', { className: "user" },
+        Image(user.profileImageURL), Handle(user.twitterHandle), Name(user.name));
+}
+
+class TweetList extends React.Component {
     render() {
-        return e('div', { className: "item" },
-            this.props.user, this.props.message);
+        return this.props.jsonList.map(i => e('div', { className: "item" }, User(i.user), Message(i)));
     }
 }
 
 const blockify = (s) => {
     let json = JSON.parse(s);
-
-    let timelineContainer = [];
-    let dataContainer = document.getElementsByClassName("data") && document.getElementsByClassName("data")[0];
-
-    for(let i in json) {
-        if(json[i] == null) { break; }
-
-        let jsonObj = json[i];
-
-        let messageContainer, userContainer, divContainer;
-
-        /* Create the User */
-        if(jsonObj.user != null) {
-            if(jsonObj.user.twitterHandle != null && jsonObj.user.name != null
-                && jsonObj.user.profileImageURL != null) {
-                userContainer = e(User, {
-                    srcURL: jsonObj.user.profileImageURL,
-                    handle: jsonObj.user.twitterHandle,
-                    name: jsonObj.user.name}, null);
-            }
-        }
-
-        /* Create the Message */
-        if(jsonObj.createdAt != null && jsonObj.twitterMessage != null
-            && jsonObj.id != null) {
-            let msgDate = new Date(jsonObj.createdAt);
-            let formatter = new Intl.DateTimeFormat('eng', { month: "short"});
-
-            messageContainer = e(Message, {
-                    date: formatter.format(msgDate) + " " + msgDate.getUTCDate(),
-                    link: ("https://twitter.com/" + jsonObj.user.name + "/status/" + jsonObj.id),
-                    target: "_blank",
-                    message: jsonObj.twitterMessage}, null);
-        }
-
-        divContainer = e(Tweet, { user: userContainer, message: messageContainer}, null);
-        timelineContainer.push(divContainer);
-    }
-
+    let timelineContainer = e(TweetList, {jsonList: json});
     return timelineContainer;
 }
