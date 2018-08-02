8ed96df19c9b6b86c5a6370aa1a57d3f3c917491
Andrew <andrewcchau@gmail.com>
2018-08-02 09:30:55 -0500

    Added valid test for TweetList

--- src/test/TweetList.test.js
+++ src/test/TweetList.test.js
@@ -3,8 +3,14 @@ import {shallow} from 'enzyme';
 import React from 'react';
 import {Pending, Error, Button, TweetList} from '../js/components/TweetList';
 
-let message = "button message";
 const e = React.createElement;
+
+let message = "button message",
+    handle = "TesterHandle",
+    name = "Tester",
+    img = "https://http.cat/404",
+    date = "December 17, 1995 03:24:00",
+    id = "123456";
 
 /* Test Pending Component */
 describe('Pending Test', () => {
@@ -63,9 +69,44 @@ describe('Button Test Valid Inputs + Simulate Click', () => {
 
 
 /* Test TweetList Component */
+const callBackFunc = (cb) => {
+    cb(jsonObj);
+}
+
+const json = (message, handle, name, img, created, id) => {
+    return '[{"twitterMessage":"' + message + '",' +
+            '"user": {' +
+                '"twitterHandle":"' + handle + '",' +
+                '"name":"' + name + '",' +
+                '"profileImageURL":"' + img + '"},' +
+            '"createdAt":"' + created + '",' +
+            '"id":"' + id + '"}]';
+}
+
+let jsonObj = JSON.parse(json(message, handle, name, img, date, id));
+
 describe('TweetList Test No Input', () => {
     it('Should have children components but nothing else', () => {
         const wrapper = shallow(e(TweetList));
         expect(wrapper.find('div').length).toEqual(3);
+        expect(wrapper.childAt(0).hasClass('buttonContainer')).toEqual(true);
+        expect(wrapper.childAt(1).hasClass('data')).toEqual(true);
+        expect(wrapper.childAt(1).children().length).toEqual(1);
+    });
+});
+
+
+describe('TweetList Test Valid Inputs and Button Press', () => {
+    it('Should have proper children components after button press', () => {
+        const wrapper = shallow(e(TweetList, { callBack: callBackFunc }));
+        expect(wrapper.find('div').length).toEqual(3);
+        wrapper.find('button').simulate('click');
+        expect(wrapper.find('div').length).toEqual(9);
+
+        let itemElem = wrapper.childAt(1).childAt(0);
+        expect(itemElem.children().length).toEqual(2);
+        expect(itemElem.hasClass('item')).toEqual(true);
+        expect(itemElem.childAt(0).hasClass('user')).toEqual(true);
+        expect(itemElem.childAt(1).hasClass('message')).toEqual(true);
     });
 });
