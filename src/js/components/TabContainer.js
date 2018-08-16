bca77d53ad9c671a1b057e61efcee83553a6ea8c
Andrew <andrewcchau@gmail.com>
2018-08-16 09:24:03 -0500

    Refactored code to render content when needed

--- src/js/components/TabContainer.js
+++ src/js/components/TabContainer.js
@@ -7,7 +7,10 @@ import PostTweetUI from './PostTweetUI';
 const e = React.createElement;
 
 const Tab = (properties) => {
-    return e('button', {className: properties.className, onClick: properties.onClick, key: properties.key}, properties.tabName);
+    return e('button', {className: properties.className,
+                        onClick: properties.onClick,
+                        key: properties.key},
+                properties.tabName);
 }
 
 let tab;
@@ -24,13 +27,6 @@ class TabContainer extends React.Component {
 
     /* Change the content to be displayed */
     openTab(event, tabName) {
-//        if(this.props.children) {
-//            let tabContent = this.props.children.filter((i) => _.isEqual(i.key, tabName));
-//            this.setState({
-//                content: tabContent,
-//                currentTab: tabName
-//            });
-//        }
         let content;
         switch(tabName) {
             case "Home Timeline":
@@ -55,23 +51,7 @@ class TabContainer extends React.Component {
     }
 
     render() {
-        /* TODO: TEST THIS THOROUGHLY!! */
         return e('div', {className: "tabContainer"},
-//                    _.map(this.props.children, (content) => {
-//                        let append = "";
-//                        if(_.isEqual(this.state.currentTab, content.key)) {
-//                            append = " active";
-//                        }
-//
-//                        let tabProp = {
-//                            className: "tab" + append,
-//                            onClick: (event) => this.openTab(event, content.key),
-//                            key: content.key + " Tab",
-//                            tabName: content.key
-//                        }
-//
-//                        return Tab(tabProp);
-//                    }),
                 _.map(this.state.tabs, (tabName) => {
                     let append = "";
                     if(_.isEqual(this.state.currentTab, tabName)) {
--- src/js/components/UI.js
+++ src/js/components/UI.js
@@ -1,23 +1,12 @@
 import React from 'react';
-import TabContainer from './Tabs';
-import HomeTimeline from './HomeTimeline';
-import UserTimeline from './UserTimeline';
-import PostTweetUI from './PostTweetUI';
+import TabContainer from './TabContainer';
 
 const e = React.createElement;
 
-const Title = (title) => {
-    return e('div', { className: "title" }, title);
-}
-
 class UI extends React.Component {
     render() {
-//        let homeTimeline = e(HomeTimeline, {key: "Home Timeline"});
-//        let userTimeline = e(UserTimeline, {key: "User Timeline"});
-//        let postUI = e(PostTweetUI, {key: "Post Tweet"})
-
         return e('div', {},
-                Title("Lab for Andrew"),
+                e('div', { className: "title" }, "Lab for Andrew"),
                 e(TabContainer));
     }
 }
